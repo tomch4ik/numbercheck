b981de12193a63b00de71d5dca1e3251bfc80f61
Samof_qs56@student.itstep.org <Samof_qs56@student.itstep.org>
2024-11-17 18:40:33 +0200

    delegates

---
+++ numbercheck/Datecheck.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numbercheck
+{
+    internal class Datecheck
+    {
+        public delegate void TimeDelegate();
+        public delegate double AreaDelegate(double a, double b);
+        static void CurrentTime()
+        {
+            Console.WriteLine(DateTime.Now.ToString("HH:mm:ss"));
+        }
+        static void CurrentDate()
+        {
+            Console.WriteLine(DateTime.Now.ToString("dd.MM.yyyy"));
+        }
+        static void CurrentDayOfWeek()
+        {
+            Console.WriteLine(DateTime.Now.ToString("dddd"));
+        }
+        static double Triangle(double length, double height)
+        {
+            return 0.5 * length * height;
+        }
+        static double Rectangle(double width, double height)
+        {
+            return width * height;
+        }
+        static void Main(string[] args)
+        {
+            TimeDelegate showTime = CurrentTime;
+            TimeDelegate showDate = CurrentDate;
+            TimeDelegate showDayOfWeek = CurrentDayOfWeek;
+            Console.WriteLine("Время сейчас:");
+            showTime();
+            Console.WriteLine("Дата сегодня:");
+            showDate();
+            Console.WriteLine("День недели:");
+            showDayOfWeek();
+            AreaDelegate triangle = Triangle;
+            AreaDelegate rectangle = Rectangle;
+            double lengthTriangle = 5;
+            double heightTriangle = 10;
+            Console.WriteLine($"Площадь треугольника с основанием {lengthTriangle} и высотой {heightTriangle}: {triangle(lengthTriangle, heightTriangle)}");
+            double widthRectangle = 4;
+            double heightRectangle = 8;
+            Console.WriteLine($"Площадь прямоугольника с шириной {widthRectangle} и высотой {heightRectangle}: {rectangle(widthRectangle, heightRectangle)}");
+        }
+
+      
+    }
+}
---
+++ numbercheck/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace numbercheck
+{
+    delegate bool Number(int number);
+
+    class Program
+    {
+        public static bool EvenNumber(int number)
+        {
+            return number % 2 == 0;
+        }
+        public static bool OddNumber(int number)
+        {
+            return number % 2 != 0;
+        }
+        public static bool DefaultNumber(int number)
+        {
+            if (number < 2) return false;
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0) return false;
+            }
+            return true;
+        }
+        public static bool FibonacciNumber(int number)
+        {
+            if (number < 0)
+                return false;
+            int a = 0, b = 1;
+            while (a < number)
+            {
+                int new_num = a;
+                a = b;
+                b = new_num + b;
+            }
+            return a == number;
+        }
+
+        static void Main(string[] args)
+        {
+            Number[] checks = { EvenNumber, OddNumber, DefaultNumber, FibonacciNumber };
+            int choice = 0;
+            int[] numbers = {1, 2, 3, 4, 5, 6};
+            while (choice != 5)
+            {
+                Console.WriteLine("\nВыберите проверку:");
+                Console.WriteLine("1 - Проверка на четность");
+                Console.WriteLine("2 - Проверка на нечетность");
+                Console.WriteLine("3 - Проверка на простое число");
+                Console.WriteLine("4 - Проверка на число Фибоначчи");
+                Console.WriteLine("5 - Выход");
+                Console.Write("Ваш выбор: ");
+                choice = Convert.ToInt32(Console.ReadLine());
+                if (choice >= 1 && choice <= 4)
+                {
+                    foreach (int number in numbers)
+                    {
+                        bool result = checks[choice - 1](number);
+                        Console.WriteLine($"Число {number}: " + (result ? "Да" : "Нет"));
+                    }
+                }
+            }
+        }
+    }
+}
